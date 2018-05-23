df2a54211a45c2eac7b615843885dc7841fdc56e
Matthew Boynes <mboynes@alleyinteractive.com>
2018-05-23 12:11:14 -0400

    Scaffold boilerplate

---
+++ build-issue-example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Build Issue Example
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     build-issue-example
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Build_Issue_Example
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Build_Issue_Example
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
